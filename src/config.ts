4e57da13278b94f678325acc245f9869e08a335e
AgnetX <Abdullahesmaeal@gmail.com>
2026-06-01 20:52:43 +0300

    fix download url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // ★ عدّل الرابط ده كل ما ترفع نسخة جديدة من البرنامج
 // ★ Update this URL every time you release a new version
 export const DOWNLOAD_URL =
-  "https://github.com/YOUR_USERNAME/YOUR_REPO/releases/latest/download/StreamFetch-Setup.exe";
+  "https://github.com/Agnet-X/StreamFetch/releases/latest/download/StreamFetch.exe";
 
 // Ad timers (in seconds)
 export const WELCOME_AD_DELAY = 5;
